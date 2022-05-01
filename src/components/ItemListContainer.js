d9a6104daa708a7bd652a44d267fc2d34fd0779a
pbasualdoarcati <pbasualdoarcati@hotmail.com>
2022-05-01 20:51:33 -0300

    Componente ItemListContainer

---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,14 @@
+
+
+
+
+function ItemListContainer (){
+    return(
+        <>
+        <h1>Aqui tendremos una lista de todos los items</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis error repellendus modi fugit expedita dolorum qui odit, ratione quidem! Excepturi iusto repudiandae quas ea, necessitatibus mollitia sapiente voluptas alias numquam.
+        </p>
+        </>
+    )
+}
+export default ItemListContainer
