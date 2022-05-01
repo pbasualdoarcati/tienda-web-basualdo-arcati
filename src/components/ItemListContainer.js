687170dd82679aaa3390f7d90d4f1ed0c18c65ce
pbasualdoarcati <pbasualdoarcati@hotmail.com>
2022-05-01 20:53:11 -0300

    Componente ItemListContainer

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -8,6 +8,7 @@ function ItemListContainer (){
         <h1>Aqui tendremos una lista de todos los items</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis error repellendus modi fugit expedita dolorum qui odit, ratione quidem! Excepturi iusto repudiandae quas ea, necessitatibus mollitia sapiente voluptas alias numquam.
         </p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur reiciendis esse enim deleniti blanditiis cumque nihil error laudantium quia impedit nobis qui sint aliquid autem atque, harum sapiente quam incidunt!</p>
         </>
     )
 }
