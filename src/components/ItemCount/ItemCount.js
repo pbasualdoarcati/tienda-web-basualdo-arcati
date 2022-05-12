eab8c2dc3f5e9fa25473867f5f6653f6bff4c678
pbasualdoarcati <pbasualdoarcati@hotmail.com>
2022-05-12 00:33:40 -0300

    Corrección del componente ItemCount

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,23 +1,48 @@
 //Components
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import { useState } from 'react';
 
 //Styles
 import './ItemCount.scss'
 
-function ItemCount({stock, totalStore, onAdd, offAdd, disableButtonRest, disableButton}) {
+function ItemCount({stock, initial, onAdd}) {
+
+  let disableButtonRest = false
+  let disableButton = false
+
+  const [counterItem, setCounterItem] = useState(initial)
+
+  const handlerClickAdd = () =>{
+      setCounterItem(counterItem + 1)
+  }
+
+  const handlerClickRest = () =>{
+      setCounterItem(counterItem - 1)
+  }
+
+// Control de botones ante el stock y cantidad de items
+
+//Si el contador inicial es 0, desactivo el boton -
+if(counterItem === 0){
+  disableButtonRest = true
+}
+
+//Si el contador es mayor que el stock, desactivo el boton +
+if (counterItem >= stock){
+  disableButton = true
+}
 
   return (
     <>
     <div>
-        <p> Total store: {totalStore}</p>
+        <p> Total store: {stock - counterItem}</p>
         <div>
-            <Button variant="primary" onClick={onAdd} disabled={disableButton} className='itemButton'> + </Button> 
-                <span> {stock} </span>
-            <Button variant="primary" onClick={offAdd} disabled={disableButtonRest} className='itemButton' > - </Button>
+            <Button variant="primary" disabled={disableButton} className='itemButton' onClick={handlerClickAdd}> + </Button> 
+                <span> {counterItem} </span>
+            <Button variant="primary" disabled={disableButtonRest} className='itemButton' onClick={handlerClickRest}> - </Button>
         </div>
-        <Button variant= 'warning' className='m-2 itemButton'>Ver detalle</Button>
-        <Button variant= 'success' disabled={disableButtonRest} className='itemButton'>Agregar al Carrito</Button>
+        <Button variant= 'success' disabled={disableButtonRest} className='itemButton' onClick={onAdd}>Agregar al Carrito</Button>
 
 </div>
     
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -31,6 +31,7 @@ function ItemList() {
         })
     }, [setItems])
 
+    
   return (
     <>
     {
